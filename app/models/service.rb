3a85579a6374d2c23af84133a42c71580d24767b
Fernanda de Jesus <fernandabussular@gmail.com>
2021-05-06 20:31:53 -0300

    Edit Model Service

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,5 @@
 class Service < ApplicationRecord
+  belongs_to :user
+  CATEGORIES = %w(construction education health programing)
+  validates :category, presence: true, inclusion: { in: CATEGORIES, message: "%{value} is not permited category" }
 end
