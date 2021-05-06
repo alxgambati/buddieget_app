51e8befa968ef41f9b0b9ed929cfbe2a92fccaa4
Alexandre Gambati <gambati@hotmail.com>
2021-05-06 12:12:00 -0300

    devise views and more user info

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
+  get 'user/add_data', to: 'users#edit', as: :edit_user
+  put 'user/add_data', to: 'users#update', as: :update_user
   resources :services
-  resource :orders, only: [:new, :create] 
+  resource :orders, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
