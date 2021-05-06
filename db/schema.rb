e5b922c60565599d29ab7b01d2073d6b594863d2
Raquel Lima <raqueljones182@gmail.com>
2021-05-06 20:46:21 -0300

    add controller orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_225522) do
+ActiveRecord::Schema.define(version: 2021_05_06_231217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "service_id", null: false
+    t.string "city"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["service_id"], name: "index_orders_on_service_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "services", force: :cascade do |t|
     t.string "category"
@@ -22,6 +32,8 @@ ActiveRecord::Schema.define(version: 2021_05_05_225522) do
     t.string "city"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_services_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +52,7 @@ ActiveRecord::Schema.define(version: 2021_05_05_225522) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "services"
+  add_foreign_key "orders", "users"
+  add_foreign_key "services", "users"
 end
