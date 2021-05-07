d4bb4fefa0607db316f90df065b00e7090a905a9
Raquel Lima <raqueljones182@gmail.com>
2021-05-07 17:01:40 -0300

    orders controller and view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,14 @@
 class OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render :new
+    end
+  end
 end
