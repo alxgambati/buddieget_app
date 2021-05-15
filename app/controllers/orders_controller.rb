19ed336cd337703735a95b49ee2dd49b65bda664
Fernanda de Jesus <fernandabussular@gmail.com>
2021-05-15 09:25:25 -0300

    Change

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,7 @@ class OrdersController < ApplicationController
     @order = Order.new
     @order.user = current_user
     @order.service = Service.find(params[:service])
-    autorize @order
+    authorize @order
     if @order.save
       redirect_to dashboard_path(results_query: 'orders')
     else
